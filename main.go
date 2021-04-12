9bbcb6c808971050b3d636951e5b627aecac857f
Charlie Xu <charlie.xu@mongodb.com>
2021-04-12 13:55:13 -0400

    Restructured main.go

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 )
 
 func main() {
+	addr := initializeServer()
+	initializeService(addr)
+}
+
+func initializeServer() string {
 	// Determine web port
 	addr, err := determineListenAddress()
 	if err != nil {
@@ -17,6 +22,10 @@ func main() {
 	}
 	log.Printf("Listening on port %s...\n", addr)
 
+	return addr
+}
+
+func initializeService(addr string) {
 	// Initialize Gorilla mux
 	myRouter := mux.NewRouter().StrictSlash(true)
 
@@ -29,15 +38,15 @@ func main() {
 	}
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!  Leafylink here.\n")
-	log.Println("Someone hit the home page")
-}
-
 func determineListenAddress() (string, error) {
 	port := os.Getenv("PORT")
 	if port == "" {
 		return "", fmt.Errorf("$PORT not set")
 	}
 	return ":" + port, nil
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!  Leafylink here.\n")
+	log.Println("Someone hit the home page")
 }
