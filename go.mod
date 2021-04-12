3f0dc3d214f9e9c5765722799f88372681f902e6
Charlie Xu <charlie.xu@mongodb.com>
2021-04-12 15:25:35 -0400

    Added Atlas connection and dynamic environment variables

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module leafylink
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/kr/fs v0.1.0 // indirect
-	github.com/kr/pretty v0.2.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
-	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
-	golang.org/x/tools v0.1.0 // indirect
+	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
+	go.mongodb.org/mongo-driver v1.5.1
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var (
+	db     string
+	client *mongo.Client
+)
+
+type mapping struct {
+	createDate time.Time
+	key        string
+	redirect   string
+	useCount   int
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	addr := initializeServer()
+
+	log.Printf("Leafylink listening on port %s", addr)
+
+	client = initializeDb()
+
 	initializeService(addr)
 }
 
@@ -20,16 +46,36 @@ func initializeServer() string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Listening on port %s...\n", addr)
-
 	return addr
+}
+
+func initializeDb() *mongo.Client {
+	switch os.Getenv("ENV") {
+	case "PROD":
+		db = "leafylink_prod"
+	case "DEV":
+		db = "leafylink_dev"
+	default:
+		db = "leafylink_local"
+	}
+
+	cxnParams := "/?retryWrites=true&w=majority"
+	dbCxnString := "mongodb+srv://" + os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_URL") + "/" + db + cxnParams
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dbCxnString))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
 }
 
 func initializeService(addr string) {
 	// Initialize Gorilla mux
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	// Page handling
+	// Handlers
 	myRouter.HandleFunc("/", homePage)
 
 	// Initialize listen and serve
@@ -48,5 +94,5 @@ func determineListenAddress() (string, error) {
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!  Leafylink here.\n")
-	log.Println("Someone hit the home page")
+	log.Println("Home page served")
 }
